ff6034f70796b74a6ace409963581a70e182988f
Paul Garton <paulgarton1984@hotmail.com>
2022-09-20 21:35:46 +0100

    Add java to buttons

---
+++ script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Audio for accept & decline buttons
+document.getElementById('acceptBtn').addEventListener('click', function () {
+  document.getElementById('accept').play();
+});
+
+document.getElementById('declineBtn').addEventListener('click', function () {
+  document.getElementById('decline').play();
+});
