f3578f25991ef8436debdc2fc578e3cd369983ca
Paul Garton <paulgarton1984@hotmail.com>
2022-09-27 21:06:04 +0100

    increment counter

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ document.getElementById('acceptBtn').addEventListener('click', function () {
 document.getElementById('declineBtn').addEventListener('click', function () {
   document.getElementById('decline').play();
 });
+
+// Button click counter
+const button = document.getElementById('acceptBtn');
+const counter = document.getElementById('counter');
+var count = 0;
+
+function counterAdd() {
+  button.addEventListener('click', () => {
+    count++;
+    counter.innerHTML = count;
+  });
+}
+
+counterAdd();
