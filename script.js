3c578819602b8b565c3f37ad41d3ad2b014ab198
Paul Garton <paulgarton1984@hotmail.com>
2022-10-03 21:07:51 +0100

    various, start fun page

--- script.js
+++ script.js
@@ -24,24 +24,3 @@ function counterAdd() {
 counterAdd();
 
 // Information Scroll
-const cards = document.querySelectorAll('.card');
-
-window.addEventListener('scroll', viewCard);
-
-viewCard();
-
-function viewCard() {
-  const triggerBottom = (window.innerHeight / 5) * 4;
-
-  cards.forEach(card => {
-    const cardTop = card.getBoundingClientRect().top;
-
-    if (cardTop < triggerBottom) {
-      card.classList.add('show');
-    } else {
-      card.classList.remove('show');
-    }
-  });
-}
-
-console.log(viewCard);
