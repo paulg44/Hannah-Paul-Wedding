ac40ef6c4812c9225a54a778f17ea0a45d149d6f
Paul Garton <paulgarton1984@hotmail.com>
2022-10-09 17:41:05 +0100

    Add RSVP file, changed homepage layout

--- script.js
+++ script.js
@@ -9,18 +9,4 @@ document.getElementById('declineBtn').addEventListener('click', function () {
   document.getElementById('decline').play();
 });
 
-// Button click counter
-const button = document.getElementById('acceptBtn');
-const counter = document.getElementById('counter');
-var count = 0;
-
-function counterAdd() {
-  button.addEventListener('click', () => {
-    count++;
-    counter.innerHTML = count;
-  });
-}
-
-counterAdd();
-
 // Information Scroll
