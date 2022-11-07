ba366006e13a3656643d03998302d709f957cb03
Paul Garton <paulgarton1984@hotmail.com>
2022-11-07 18:30:06 +0000

    add shadow to cards

--- card.js
+++ card.js
@@ -1,7 +1,14 @@
 const cards = document.querySelectorAll('.card-container');
+const shadow = document.querySelectorAll('.card-shadow');
 
 cards.forEach(cards => {
   cards.addEventListener('click', function () {
     cards.classList.toggle('is-flipped');
+  });
+});
+
+shadow.forEach(shadow => {
+  shadow.addEventListener('click', function () {
+    shadow.classList.toggle('card-shadow');
   });
 });
