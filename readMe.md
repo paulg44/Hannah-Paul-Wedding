05feb2cf84aea9011463ff99a2c228ef904dbf57
paulg44 <paulgarton1984@hotmail.com>
2023-12-02 21:04:03 +0000

    repurpose website for wedding photos

---
+++ readMe.md
@@ -0,0 +1,1 @@
+This was a website originally leading up to our wedding that I am now converting into a website for people to view our official and unofficial photos and videos
--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-// Audio for accept & decline buttons
-document.getElementById('acceptBtn').addEventListener('click', function () {
-  document.getElementById('accept').play();
-});
 
-document.getElementById('declineBtn').addEventListener('click', function () {
-  document.getElementById('decline').play();
-});
