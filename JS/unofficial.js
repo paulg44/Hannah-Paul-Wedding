66eeadd9d03b4a4f22725cec8cde2776c4dd647c
paulg44 <paulgarton1984@hotmail.com>
2023-12-04 21:57:35 +0000

    add opacity to stack so only top image is full opacity

--- JS/unofficial.js
+++ JS/unofficial.js
@@ -1,4 +1,5 @@
 // Photo scroll
+// Photo array
 let photoArr = [
   {
     id: 1,
@@ -24,15 +25,69 @@ let photoArr = [
 
 const prevBtn = document.querySelector('.prev');
 const nextBtn = document.querySelector('.next');
-const photo = document.querySelector('.photo-card img');
-const descriptionP = document.querySelector('.description');
+const unofficialBtn = document.querySelector('.unofficial-btn');
+const photoContainer = document.querySelector('.photo-container');
 
 let currentPhotoIndex = 0;
 
-function photoCardChanger() {
+// Create photo element to make it look like a stack of photos
+function createPhotoCard(photo) {
+  // Create photo card
+  const photoCard = document.createElement('div');
+  photoCard.classList.add('photo-card');
+
+  //   Create image
+  const img = document.createElement('img');
+  img.src = photo.image;
+  img.alt = photo.description;
+
+  //   Create description
+  const description = document.createElement('p');
+  description.textContent = photo.description;
+
+  //   Add image and description to photo card
+  photoCard.appendChild(img);
+  photoCard.appendChild(description);
+
+  //   Add randomness to pile
+  const rotation = Math.random() * 20 - 10; // Random rotation between -10 and 10 degrees
+  const offsetX = Math.random() * 20 - 10; // Random horizontal offset between -10 and 10 pixels
+  const offsetY = Math.random() * 20 - 10; // Random vertical offset between -10 and 10 pixels
+
+  photoCard.style.transform = `rotate(${rotation}deg) translate(${offsetX}px, ${offsetY}px)`;
+
+  return photoCard;
+}
+
+// Function to initialize the photo cards
+function initializePhotoCards() {
+  photoArr.forEach(photo => {
+    const photoCard = createPhotoCard(photo);
+
+    // Add photo card to container
+    photoContainer.appendChild(photoCard);
+    //   Reduce opacity on cards lower down in the stack
+    const existingPhoto = document.querySelectorAll('.photo-card img');
+    existingPhoto.forEach((card, index) => {
+      if (index === currentPhotoIndex) {
+        card.style.opacity = 1;
+      } else {
+        card.style.opacity = 0.3;
+      }
+    });
+  });
+}
+
+// Call the initialization function
+initializePhotoCards();
+
+// Function to change photos on button click
+function photoCardChanger(photo) {
+  //   photoContainer.innerHTML = '';
+
   const currentPhoto = photoArr[currentPhotoIndex];
-  photo.src = currentPhoto.image;
-  descriptionP.textContent = currentPhoto.description;
+  const photoCard = createPhotoCard(currentPhoto);
+  photoContainer.appendChild(photoCard);
 }
 
 prevBtn.addEventListener('click', () => {
@@ -57,8 +112,10 @@ nextBtn.addEventListener('click', () => {
   photoCardChanger();
 });
 
+// Clear page
+unofficialBtn.addEventListener('click', () => {
+  console.log('unoff clicked');
+  photoContainer.innerHTML = '';
+});
+
 photoCardChanger();
-
-prevBtn.addEventListener('click', () => {});
-
-nextBtn.addEventListener('click', () => {});
